037afa45a54f26964e303a5cf1affec506af6849
Maxence Lelandais <misterduflair@gmail.com>
2024-02-22 11:04:58 +0100

    init

---
+++ backend/src/main/java/utbm/eternaldynasties/EternalDynastiesApplication.java
@@ -0,0 +1,13 @@
+package utbm.eternaldynasties;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EternalDynastiesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EternalDynastiesApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/utbm/eternaldynasties/EternalDynastiesApplicationTests.java
@@ -0,0 +1,13 @@
+package utbm.eternaldynasties;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EternalDynastiesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
