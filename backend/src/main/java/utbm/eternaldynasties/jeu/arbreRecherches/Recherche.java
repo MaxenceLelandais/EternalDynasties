9b9ecac7c0cded3ac7816ccd93e52a08575a3093
Maxence Lelandais <misterduflair@gmail.com>
2024-02-26 20:37:56 +0100

    Fin arbre de recherches (côté logique)

--- backend/src/main/java/utbm/eternaldynasties/jeu/arbreRecherches/Recherche.java
+++ backend/src/main/java/utbm/eternaldynasties/jeu/arbreRecherches/Recherche.java
@@ -13,6 +13,7 @@ public class Recherche {
     private Map<String, Recherche> debloque = new HashMap<>();
     private Map<String, String> bonus = new HashMap<>();
     private Map<String, Recherche> dependances = new HashMap<>();
+    private Map<ArrayList<Recherche>, ArrayList<Recherche>> conditions = new HashMap<>();
     private Map<String, Recherche> inhibe = new HashMap<>();
     private Boolean etat = false;
     private Boolean recherchePossible = true;
@@ -33,14 +34,16 @@ public class Recherche {
         this.jsonObjet = jsonObjet;
         return update(listeRecherches);
     }
-    public Recherche update(Map<String, Recherche> listeRecherches) {
-        this.description = this.jsonObjet.get("Description").toString();
-        this.cout = (Map<String, Long>) this.jsonObjet.get("Coût");
-        this.bonus = (Map<String, String>) this.jsonObjet.get("Bonus");
 
-        ajoutVal("Dépendance",this.dependances, listeRecherches);
-        ajoutVal("Débloque",this.debloque, listeRecherches);
-        ajoutVal("Inhibe",this.inhibe, listeRecherches);
+    public Recherche update(Map<String, Recherche> listeRecherches) {
+        this.description = (String)this.jsonObjet.get("Description");
+        this.cout = this.jsonObjet.containsKey("Coût") ? (Map<String, Long>) this.jsonObjet.get("Coût") : null;
+        this.bonus = this.jsonObjet.containsKey("Bonus") ? (Map<String, String>) this.jsonObjet.get("Bonus") : null;
+
+        ajoutVal("Dépendance", this.dependances, listeRecherches);
+        ajoutVal("Débloque", this.debloque, listeRecherches);
+        ajoutVal("Inhibe", this.inhibe, listeRecherches);
+        ajoutValIf("If", listeRecherches);
 
         this.jsonObjet.put("Etat", this.etat);
         this.jsonObjet.put("Recherche possible", this.recherchePossible);
@@ -48,19 +51,44 @@ public class Recherche {
         return this;
     }
 
-    private void ajoutVal(String key, Map<String, Recherche> liste, Map<String, Recherche> listeRecherches){
-        if (this.jsonObjet.get(key) != null) {
+    private void ajoutVal(String key, Map<String, Recherche> liste, Map<String, Recherche> listeRecherches) {
+        if (this.jsonObjet.containsKey(key) && this.jsonObjet.get(key) != null) {
             for (String valeur : (ArrayList<String>) this.jsonObjet.get(key)) {
-                Recherche recherche;
-                if (listeRecherches.containsKey(valeur)) {
-                    recherche = listeRecherches.get(valeur);
-                } else {
-                    recherche = new Recherche(valeur);
-                    listeRecherches.put(valeur, recherche);
-                }
-                liste.put(valeur, recherche);
+                liste.put(valeur, ajoutKeyValListeRecherches(listeRecherches,valeur));
             }
         }
+    }
+
+
+    private void ajoutValIf(String key, Map<String, Recherche> listeRecherches) {
+        if (this.jsonObjet.containsKey(key) && this.jsonObjet.get(key) != null) {
+            Map<String, String> map = (Map<String, String>) this.jsonObjet.get(key);
+            for (String recherchesDependantes : map.keySet()) {
+                ArrayList<Recherche> listeDependance = new ArrayList<>();
+                ArrayList<Recherche> listeDebloque = new ArrayList<>();
+
+                for (String rechercheDependante : recherchesDependantes.split("\\+")) {
+                    listeDependance.add(ajoutKeyValListeRecherches(listeRecherches, rechercheDependante));
+                }
+                for (String rechercheDebloque : map.get(recherchesDependantes).split("\\+")) {
+                    listeDebloque.add(ajoutKeyValListeRecherches(listeRecherches, rechercheDebloque));
+                }
+                this.conditions.put(listeDependance, listeDebloque);
+            }
+        }
+    }
+
+    private Recherche ajoutKeyValListeRecherches(Map<String, Recherche> listeRecherches, String valeur) {
+        Recherche recherche = null;
+        if (!valeur.isEmpty()) {
+            if (listeRecherches.containsKey(valeur)) {
+                recherche = listeRecherches.get(valeur);
+            } else {
+                recherche = new Recherche(valeur);
+                listeRecherches.put(valeur, recherche);
+            }
+        }
+        return recherche;
     }
 
 
@@ -92,14 +120,26 @@ public class Recherche {
         return etat;
     }
 
-    public void activer(){
-        if(actualiseEtat()){
+    public void activer() {
+        if (actualiseEtat() && checkConditions()) {
             this.etat = true;
             this.inhibe.values().forEach(Recherche::disableRecherche);
         }
     }
 
-    public boolean actualiseEtat(){
+    private boolean checkConditions() {
+        if (this.conditions.isEmpty()) {
+            return true;
+        }
+        for (ArrayList<Recherche> dependances : this.conditions.keySet()) {
+            if (dependances.stream().allMatch(Recherche::getEtat)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean actualiseEtat() {
         return dependances.values().stream().allMatch(Recherche::getEtat);
     }
 
@@ -119,7 +159,7 @@ public class Recherche {
         this.recherchePossible = false;
     }
 
-    public String toString(){
+    public String toString() {
         return Json.jsonMapToString(jsonObjet);
     }
 
