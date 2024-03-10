28febb643188bae222c98aabcd0dadb438acd17c
Daniel RAHMEH <danielr.97@hotmail.fr>
2024-03-10 16:10:21 +0100

    Page démarrage + choix-environnement fonctionnel

--- frontend/src/app/menu-environnement/menu-environnement.component.ts
+++ frontend/src/app/menu-environnement/menu-environnement.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { JeuService } from '../http/jeuService';
-import {Environnement, Environnements } from '../model/environnement.model'
+import { Environnement, Environnements } from '../model/environnement.model';
 
 @Component({
   selector: 'app-menu-environnement',
@@ -9,6 +9,9 @@ import {Environnement, Environnements } from '../model/environnement.model'
 })
 export class MenuEnvironnementComponent implements OnInit {
   listeEnvironnements: Environnements | null = null;
+  selectedEnvironment: Environnement | null = null;
+  hoveredKey: string | null = null;
+  selectedKey: string | null = null;
 
   constructor(private jeuService: JeuService) {}
 
@@ -22,8 +25,30 @@ export class MenuEnvironnementComponent implements OnInit {
         this.listeEnvironnements = data;
       },
       error => {
-        console.error("Erreur lors de la récupération de listeEnvironnements", error);
+        console.error("Erreur lors de la récupération des environnements", error);
       }
     );
   }
+
+  setSelectedEnvKey(key: string): void {
+    this.selectedKey = key;
+    if (this.listeEnvironnements) {
+      this.selectedEnvironment = this.listeEnvironnements[key];
+    }
+  }
+
+  setHoveredKey(key: string | null): void {
+    this.hoveredKey = key;
+  }
+
+  getEnvironmentInfo(key: string | null, infoType: 'nom' | 'description'): string {
+    if (key === null) {
+      return ''; // Retourne une chaîne vide si la clé est null
+    }
+    const env = this.listeEnvironnements ? this.listeEnvironnements[key] : null;
+    return env ? env[infoType] : '';
+  }
+  
 }
+
+
