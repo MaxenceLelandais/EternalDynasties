ea604c809e8d7654a628f018338de6baf2307253
Daniel RAHMEH <danielr.97@hotmail.fr>
2024-03-20 18:24:59 +0100

    (NE PAS TOUCHER) Arborescence des Recherches Intégrée

--- frontend/src/app/game/modal-arbre-recherche/modal-arbre-recherche.component.ts
+++ frontend/src/app/game/modal-arbre-recherche/modal-arbre-recherche.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { JeuService } from 'src/app/http/jeuService';
 import { Recherches } from 'src/app/model/recherche.model';
+import { ArbreRecherche } from 'src/app/model/arbreRecherche.model';
 import { ModalService } from 'src/app/service/modal.Service';
 
 @Component({
@@ -12,6 +13,7 @@ import { ModalService } from 'src/app/service/modal.Service';
 export class ModalArbreRechercheComponent implements OnInit {
   listeRecherches: Recherches | null = null;
   arbreRecherches: Recherches | null = null;
+  arborescenceRecherche: ArbreRecherche[] = []; 
   displayModal: boolean = false;
   
 
@@ -38,10 +40,44 @@ export class ModalArbreRechercheComponent implements OnInit {
       }
     );
   }
+  
+  construireArborescence() {
+    if (this.arbreRecherches === null) {
+      console.warn("arbreRecherches est null, impossible de construire l'arborescence");
+      return;
+    }
+  
+    const map = new Map<number, ArbreRecherche>();
+  
+    // Initialisation de la map avec des éléments transformés de Recherche à ArbreRecherche
+    Object.values(this.arbreRecherches).forEach(recherche => {
+      map.set(recherche.id, { id: recherche.id, nom: recherche.nom, enfant: [] });
+    });
+  
+    Object.values(this.arbreRecherches).forEach(recherche => {
+      const arbreRechercheTransforme = map.get(recherche.id);
+      if (recherche.parent !== null && map.has(recherche.parent)) {
+        const parentArbre = map.get(recherche.parent);
+        if (parentArbre && arbreRechercheTransforme) {
+          parentArbre.enfant.push(arbreRechercheTransforme); // Ici on pousse bien un ArbreRecherche
+        }
+      } else {
+        // C'est une racine, donc on l'ajoute directement à arborescenceRecherche si elle existe
+        if (arbreRechercheTransforme) {
+          this.arborescenceRecherche.push(arbreRechercheTransforme);
+        }
+      }
+    });
+  }
+  
+
 
   ngOnInit() {
     this.modalService.watch().subscribe((status: 'open' | 'close') => {
       this.displayModal = status === 'open';
+      if(this.displayModal) {
+        this.construireArborescence(); // Appel de la fonction pour construire l'arborescence
+      }
     });
   }
 
