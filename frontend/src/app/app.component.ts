81f2ef921bb56fa2c10c501aa86a14623277a688
nicochess90 <116639206+nicochess90@users.noreply.github.com>
2024-03-27 11:33:09 +0100

    prepare environnement musique

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,45 +1,64 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Environnement } from './model/environnement.model';
+import { EnvironnementService } from './service/environnementService';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
-  audioFiles: string[] = []; // Array to hold the list of audio files
-  currentAudioIndex: number = 0; // Index of the currently playing audio file
+  audioFiles: string[] = [];
+  currentAudioIndex: number = 0;
+  environnement: Environnement | null = null;
 
-  constructor() { }
+  constructor(private environnementService: EnvironnementService) {}
 
   ngOnInit(): void {
-    this.loadAudioFiles(); // Load the list of audio files when the component initializes
+    this.loadAudioFiles();
+    this.loadEnvironnement();
   }
 
   loadAudioFiles() {
-    // Assuming you have an array of file names in your assets folder
-    this.audioFiles = ['1.mp3', '2.mp3', '3.mp3', '4.mp3']; // Add all your audio file names here
+    this.audioFiles = ['1.mp3', '2.mp3', '3.mp3', '4.mp3'];
+  }
+
+  loadEnvironnement() {
+    if (localStorage.getItem('environnement')) {
+      const savedEnv = localStorage.getItem('environnement');
+      if (savedEnv != null) {
+        this.environnement = JSON.parse(savedEnv);
+      }
+    } else {
+      console.log("savedEnv");
+      console.log("savedEnv");
+      this.environnementService.currentEnvironnement.subscribe(environnement => {
+        this.environnement = environnement;
+        localStorage.setItem('environnement', JSON.stringify(environnement));
+      });
+    }
   }
 
   playRandomAudio() {
-    const randomIndex = Math.floor(Math.random() * this.audioFiles.length); // Generate a random index
-    this.currentAudioIndex = randomIndex; // Update the current audio index
+    const randomIndex = Math.floor(Math.random() * this.audioFiles.length);
+    this.currentAudioIndex = randomIndex;
     const audioPlayer = document.getElementById('audioPlayer') as HTMLAudioElement;
-    audioPlayer.src = `assets/music/${this.audioFiles[randomIndex]}`; // Set the source of the audio player
-    audioPlayer.play(); // Play the audio
+    audioPlayer.src = `assets/music/${this.audioFiles[randomIndex]}`;
+    audioPlayer.play();
   }
 
   nextAudio() {
-    this.currentAudioIndex = (this.currentAudioIndex + 1) % this.audioFiles.length; // Move to the next audio file
+    this.currentAudioIndex = (this.currentAudioIndex + 1) % this.audioFiles.length;
     const audioPlayer = document.getElementById('audioPlayer') as HTMLAudioElement;
-    audioPlayer.src = `assets/music/${this.audioFiles[this.currentAudioIndex]}`; // Set the source of the audio player
-    audioPlayer.play(); // Play the audio
+    audioPlayer.src = `assets/music/${this.audioFiles[this.currentAudioIndex]}`;
+    audioPlayer.play();
   }
 
   previousAudio() {
-    this.currentAudioIndex = (this.currentAudioIndex - 1 + this.audioFiles.length) % this.audioFiles.length; // Move to the previous audio file
+    this.currentAudioIndex = (this.currentAudioIndex - 1 + this.audioFiles.length) % this.audioFiles.length;
     const audioPlayer = document.getElementById('audioPlayer') as HTMLAudioElement;
-    audioPlayer.src = `assets/music/${this.audioFiles[this.currentAudioIndex]}`; // Set the source of the audio player
-    audioPlayer.play(); // Play the audio
+    audioPlayer.src = `assets/music/${this.audioFiles[this.currentAudioIndex]}`;
+    audioPlayer.play();
   }
 }
