d9ffc38753eae23c33ad61499123758cc8500a94
Daniel RAHMEH <danielr.97@hotmail.fr>
2024-04-07 14:11:05 +0200

    (NE PAS TOUCHER) Frise chronologique fonctionnelle

--- frontend/src/app/game/frise-chronologique/frise-chronologique.component.ts
+++ frontend/src/app/game/frise-chronologique/frise-chronologique.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-import { Eres } from 'src/app/model/ere.model';
+import { Ere, Eres } from 'src/app/model/ere.model';
 import { JeuService } from 'src/app/http/jeuService';
+import { NomJoueurService } from 'src/app/service/nomJoueurService';
 
 
 @Component({
@@ -10,8 +11,16 @@ import { JeuService } from 'src/app/http/jeuService';
 })
 export class FriseChronologiqueComponent {
   listeEre: Eres | null = null;
+  nomJoueur: string | null = null;
+  ere: Ere | null = null;
 
-  constructor(private jeuService: JeuService) {this.fetchData()}
+  constructor(private jeuService: JeuService, private nomJoueurService: NomJoueurService) {}
+
+  ngOnInit() {
+    this.nomJoueur = this.nomJoueurService.getNomJoueur();
+    this.fetchData()
+    console.log("nom pour ere : " + this.nomJoueur);
+  }
 
   fetchData() {
     this.jeuService.httpListeEres().subscribe(
@@ -23,6 +32,18 @@ export class FriseChronologiqueComponent {
         console.error("Erreur lors de la récupération des eres", error);
       }
     );
+    console.log("bonjour0 : " + this.nomJoueur);
+    if (this.nomJoueur) {
+      this.jeuService.httpEre(this.nomJoueur).subscribe({
+        next: (response) => {
+          this.ere = response;
+          console.log('Réponse ere', response);
+        },
+        error: (error) => {
+          console.error('Erreur lors de la requête', error);
+        }
+      });
+    }
   }
 }
 
--- frontend/src/app/music/music.component.ts
+++ frontend/src/app/music/music.component.ts
@@ -1,9 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';;
 import { Environnement } from '../model/environnement.model';
 import { EnvironnementService } from '../service/environnementService';
-import { EreService } from '../service/ere.service'; 
-import { JeuService } from '../http/jeuService';
-import { CivilisationService } from '../service/civilisationService';
 
 @Component({
   selector: 'app-music',
@@ -16,20 +13,12 @@ export class MusicComponent implements OnInit {
   audioFiles: string[] = [];
   currentAudioIndex: number = 0;
   environnement: Environnement | null = null;
-  ereActuelle: string = ""; // Variable pour stocker l'ère actuelle
-  civilisation:any;
 
-  constructor(
-    private environnementService: EnvironnementService,
-    private ereService: EreService,
-    private jeuService: JeuService, 
-    private civilisationService: CivilisationService // Injection du service EreService
-  ) {}
-  
+  constructor(private environnementService: EnvironnementService) {}
+
   ngOnInit(): void {
     this.loadAudioFiles();
     this.loadEnvironnement();
-    this.loadEreActuelle(); // Appel à la méthode pour charger l'ère actuelle
   }
 
   loadAudioFiles() {
@@ -39,62 +28,45 @@ export class MusicComponent implements OnInit {
   loadEnvironnement() {
     if (localStorage.getItem('environnement')) {
       const savedEnv = localStorage.getItem('environnement');
-      if (savedEnv !== null) {
+      if (savedEnv != null) {
         this.environnement = JSON.parse(savedEnv);
       }
     } else {
-      this.environnementService.currentEnvironnement.subscribe((environnement: Environnement | null) => {
-        if (environnement !== null) {
-          this.environnement = environnement;
-          localStorage.setItem('environnement', JSON.stringify(environnement));
-        }
+      console.log("savedEnv");
+      console.log("savedEnv");
+      this.environnementService.currentEnvironnement.subscribe(environnement => {
+        this.environnement = environnement;
+        localStorage.setItem('environnement', JSON.stringify(environnement));
       });
     }
   }
-  
-  
-
-  loadEreActuelle() {
-    if(this.civilisationService.getCivilisation()!=null){
-      this.civilisation = this.civilisationService.getCivilisation();
-      this.jeuService.httpEreActuelle(this.civilisation.nom+"-"+this.civilisation.nomEnvironnement).subscribe(
-        ere => {
-          this.ereActuelle = ere["nom"];
-          localStorage.setItem('ereActuelle', ere["nom"]);
-        },
-        error => {
-          console.error("Erreur lors de l'ajout de la ressource", error);
-        }
-      );
-    }
-  }
-  
 
   playRandomAudio() {
     if (this.environnement) {
         const randomIndex = Math.floor(Math.random() * this.audioFiles.length);
         this.currentAudioIndex = randomIndex;
-        this.playAudioAtIndex(randomIndex);
+        const audioPlayer = document.getElementById('audioPlayer') as HTMLAudioElement;
+        audioPlayer.src = `assets/music/${this.environnement.nom}/${this.audioFiles[randomIndex]}`;
+        audioPlayer.play();
     }
-  }
+}
 
-  nextAudio() {
+nextAudio() {
     if (this.environnement) {
         this.currentAudioIndex = (this.currentAudioIndex + 1) % this.audioFiles.length;
-        this.playAudioAtIndex(this.currentAudioIndex);
+        const audioPlayer = document.getElementById('audioPlayer') as HTMLAudioElement;
+        audioPlayer.src = `assets/music/${this.environnement.nom}/${this.audioFiles[this.currentAudioIndex]}`;
+        audioPlayer.play();
     }
-  }
+}
 
-  previousAudio() {
+previousAudio() {
     if (this.environnement) {
         this.currentAudioIndex = (this.currentAudioIndex - 1 + this.audioFiles.length) % this.audioFiles.length;
-        this.playAudioAtIndex(this.currentAudioIndex);
+        const audioPlayer = document.getElementById('audioPlayer') as HTMLAudioElement;
+        audioPlayer.src = `assets/music/${this.environnement.nom}/${this.audioFiles[this.currentAudioIndex]}`;
+        audioPlayer.play();
     }
-  }
+}
 
-  private playAudioAtIndex(index: number) {
-    const audioPlayer = document.getElementById('audioPlayer') as HTMLAudioElement;
-    audioPlayer.src = `assets/music/${this.environnement?.nom}/${this.ereActuelle}/${this.audioFiles[index]}`;
-    audioPlayer.play();
-  }
 }
