08e8128661a17450446127ef654009979ae57158
Maxence Lelandais <misterduflair@gmail.com>
2024-04-16 21:31:14 +0200

    correction mineur et bucheron

--- compiler.bat
+++
@@ -1,1 +0,0 @@
-cd frontend && ng build && cd .. && rmdir /Q /S backend\src\main\resources\static && mkdir backend\src\main\resources\static && xcopy frontend\dist\eternal-dynasties backend\src\main\resources\static /s /e && cd backend && mvn clean package -DskipTests=true
--- execute.bat
+++
@@ -1,2 +0,0 @@
-start firefox localhost:9876/index.html
-java -jar backend/target/eternal-dynasties-1.0.0.jar
